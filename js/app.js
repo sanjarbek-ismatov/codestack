9d49635f2148229ec300e89ab3eb42a27d965389
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-02-22 22:47:19 +0500

    updated

--- js/app.js
+++ js/app.js
@@ -5,6 +5,18 @@ const show = () => {
     navigation.style.display === 'none' ? navigation.style.display = 'block' : navigation.style.display = 'none';
 }
 if(document.body.style.minWidth === '1000px'){
-    navigation.style.display = 'flex'
+    let style = document.createElement('style');
+    style.innerHtml =  `{
+            .nav-ul {
+              display: flex;
+              list-style: none;
+            }
+            .nav-ul li {
+              padding: 12px 30px;
+              font-size: 25px;
+              transition: 0.3s ease;
+              border-radius: 5px;
+            }
+        }`
 }
-menu.addEventListener('click', show)
+menu.addEventListener('click', show())
