8018641c747d47c1739c8cbbeb08ad26af81da4f
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-02-23 19:49:29 +0500

    updated

--- js/app.js
+++ js/app.js
@@ -1,22 +1,15 @@
 const menu = document.getElementById('menu');
 const navigation = document.getElementById('ul');
+navigation.style.display = 'flex';
+let windows = window.matchMedia('(max-width: 1000px)')
+if(windows.matches){
+    navigation.style.display = 'none'
+}
 
 const show = () => {
-    navigation.style.display === 'none' ? navigation.style.display = 'block' : navigation.style.display = 'none';
+    console.log(navigation.style.display)
+    navigation.style.display === 'none' ? navigation.style.display =  'block' : navigation.style.display =  'none';
 }
-if(document.body.style.minWidth === '1000px'){
-    let style = document.createElement('style');
-    style.innerHtml =  `{
-            .nav-ul {
-              display: flex;
-              list-style: none;
-            }
-            .nav-ul li {
-              padding: 12px 30px;
-              font-size: 25px;
-              transition: 0.3s ease;
-              border-radius: 5px;
-            }
-        }`
-}
-menu.addEventListener('click', show())
+
+
+menu.addEventListener('click', show);
