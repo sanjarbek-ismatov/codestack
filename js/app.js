8b4f1fb0b245d80be09aad79eb257e26518407db
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-03-03 20:14:22 +0500

    succes

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@ const menu = document.getElementById("menu");
 const navigation = document.getElementById("ul");
 const buttons = document.querySelectorAll("#a");
 const logo = document.getElementById("logo");
+
+const interval = setInterval(clock, 1000);
+
+const data = document.querySelector("#copyright");
+function clock() {
+  const date = new Date();
+  data.innerHTML = `${date.toDateString()} <br> ${date.toLocaleTimeString()} <br> Sanjarbek Ismatov ${date.getFullYear()} &copy`;
+}
+
 const show = () => {
   if (navigation.className === "nav-ul") {
     navigation.className += " responsive";
@@ -21,6 +30,7 @@ const media = window.matchMedia("(min-width: 1000px)");
 const mediaFunction = () => {
   if (media.matches) {
     navigation.className = "nav-ul not-responsive";
+    logo.className = "logo-image";
   } else {
     navigation.className = "nav-ul";
   }
